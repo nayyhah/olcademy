d04e3f78c304b186c36f78387439305c9232532f
Neha Jha <70996315+nayyhah@users.noreply.github.com>
2021-08-20 01:18:33 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  session_start();
+  $count = 0;
+  // connecto database
+
+  $title = "Index";
+  require_once "./template/header.php";
+  require_once "./functions/database_functions.php";
+  $conn = db_connect();
+  $row = select4LatestBook($conn);
+?>
+
+<?php
+  if(isset($conn)) {mysqli_close($conn);}
+  require_once "./template/footer.php";
+?>
