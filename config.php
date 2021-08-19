642817722afa4e4520b5672a74aff56225e56b32
Neha Jha <70996315+nayyhah@users.noreply.github.com>
2021-08-20 01:46:27 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'sql208.epizy.com');
-define('DB_USERNAME', 'epiz_29478706');
-define('DB_PASSWORD', 'OW2dUS72rI');
-define('DB_NAME', 'epiz_29478706_taskdb');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'taskdb');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
